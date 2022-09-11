72fb7ad307fb5740dfede1036e8293db33b006ef
mdparvex <mdparvex443@gmail.com>
2022-09-11 22:33:03 +0600

    bb

--- onnorokom/settings.py
+++ onnorokom/settings.py
@@ -49,6 +49,9 @@ AUTH_USER_MODEL = 'videoshare.CustomUser' # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
